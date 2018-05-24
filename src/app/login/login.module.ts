90b5c373e1b33e998e2f26f1fa99e1960d949209
Daniel <mytechde@outlook.com>
2018-05-24 21:28:17 +0200

    WIP

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -7,8 +13,14 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule
   ],
   declarations: [LoginPageComponent]
 })
-export class LoginModule { }
+export class LoginModule {}
